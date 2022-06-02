894b72c213f95340a8dcf67702a6968e8f0ce353
Arsen Manukjan <arsenparsen1970@mail.ru>
2022-06-02 13:57:17 +0300

    loon query33

---
+++ SQLQuery33.sql
@@ -0,0 +1,10 @@
+delete from EmployeeTrigger where Id = 7
+--- 10 tund SQL
+select * from EmployeeTrigger
+select * from vEmployeeDetails
+
+
+update vEmployeeDetails
+set DeptName = 'Payroll'
+where Id = 2
+
