6fcfd31affc8f40014e6082dfdadca3cfeb69b7d
EgorKiryachkov <egor00015@gmail.com>
2022-06-02 14:59:45 +0300

    Trigger file 2 was added

---
+++ Trigger2.sql
@@ -0,0 +1,13 @@
+﻿create trigger EmployeeForDelete
+on EmployeeTrigger
+for delete
+as begin
+	declare @Id int
+	select @Id = Id from deleted
+	
+	insert into EmployeeAudit
+	values('An existing employee with Id = ' + CAST(@Id as nvarchar(5))
+	+ ' is deleted at ' + cast(GETDATE()as nvarchar(20)))
+end
+
+--Добавляет запись о удалении данных в таблицу EmployeeAudit
