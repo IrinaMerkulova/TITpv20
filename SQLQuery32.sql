b5ce7fd98dcff47442d37520b264dbc316e04377
NikitaArsenjev <nikita.arsz@gmail.com>
2022-06-02 13:49:57 +0300

    SqlQuery32 on lisatud

---
+++ SQLQuery32.sql
@@ -0,0 +1,21 @@
+create trigger trEmployeeDetails1InsteadOfInsert
+on vEmployeeDetails1
+instead of insert
+as begin
+	declare @DeptId int
+
+	select @DeptId = Department.Id
+	from Department 
+	join inserted
+	on inserted.DeptName = Department.DeptName
+
+	if(@DeptId is null)
+	begin
+		raiserror('Invalid Department Name. Statement terminated', 16, 1)
+		return
+	end
+
+	insert into EmployeeTrigger(Id, Name, Gender, DepartmentId)
+	select Id, Name, Gender, @DeptId
+	from inserted
+end
