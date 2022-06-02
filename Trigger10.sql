1409e96b7f48e335aafc9be92ad9e4f9e9397d90
StepanovMihhail <stepanovmihhail@gmail.com>
2022-06-02 13:55:17 +0300

    Trigger10

---
+++ Trigger10.sql
@@ -0,0 +1,11 @@
+create trigger trEmployeeDetails_InsteadOfDelete
+on vEmployeeDetails
+instead of delete
+as begin
+	delete EmployeeTrigger
+	from EmployeeTrigger
+	join deleted
+	on EmployeeTrigger.Id = deleted.Id
+end
+
+delete from vEmployeeDetails where Id = 2
