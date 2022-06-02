77605131947f034795d5ebf2cdb33461232774c7
AleksandrNikolajev <Nikola@gmail.com>
2022-06-02 15:02:07 +0300

    sisestame andmed

---
+++ SQLQueryTrigger1sql.sql
@@ -0,0 +1,16 @@
+create table Department
+(
+Id int primary key,
+DepartmentName nvarchar(50),
+Location nvarchar(50),
+DepartmentHead nvarchar(50)
+)
+
+create table Employees
+(
+Id int primary key,
+Name nvarchar(50),
+Gender nvarchar(10),
+Salary nvarchar(50),
+DepartmentId int
+)
