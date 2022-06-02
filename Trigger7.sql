9348dd58ec04224f797478ab4c7997218dac55ce
StepanovMihhail <stepanovmihhail@gmail.com>
2022-06-02 13:44:31 +0300

    Trigger7

---
+++ Trigger7.sql
@@ -0,0 +1,8 @@
+select * from EmployeeTrigger
+
+update EmployeeTrigger set Name = 'Todd123', Salary = 3456,
+Gender = 'Female', DepartmentId = 3 
+where Id = 4
+
+select * from EmployeeTrigger
+select * from EmployeeAudit
